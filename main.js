29043f13ea5ac8bb018af1488246de5863d29b13
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-12 10:27:26 -0300

    Printing the field to the console

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(field) {
+    this.field = field;
+    this.hat = hat;
+    this.hole = hole;
+    this.fieldCharacter = fieldCharacter;
+    this.pathCharacter = pathCharacter;
+  }
+
+  print() {
+    const joinedField = this.field.forEach((row) => {
+      console.log(row.join(''));
+    });
+    return joinedField;
+  }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
